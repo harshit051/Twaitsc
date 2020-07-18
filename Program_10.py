6749b09c04b0848c6c8d71be5c1a430fdb04ceb2
harshit051 <54814874+harshit051@users.noreply.github.com>
2020-07-18 18:15:42 +0530

    Delete Program_10.py

--- Program_10.py
+++
@@ -1,8 +0,0 @@
-k = int(input("Enter the numbers of list:"))
-CompList = []
-for i in range(k):
-    TempList = list(map(int,input().split()))
-    TempList.sort()
-    CompList.extend(TempList)
-
-print(sorted(CompList))
