f7f5e30727909a83612728a404ca25e61b3f0b7e
harshit051 <54814874+harshit051@users.noreply.github.com>
2020-07-18 18:13:15 +0530

    Delete Program_13.py

--- Program_13.py
+++
@@ -1,8 +0,0 @@
-from itertools import combinations
-arr = list(map(float,input().split()))
-comb = combinations(array,3)
-l = []
-for a,b,c in comb:
-    if 1<(a+b+c)<2:
-        l.append((a,b,c))
-print(l)
